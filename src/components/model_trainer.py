9cf3e2c1619be11bf87695ca15660712acee87de
DeveshChaturvedi <deveshchaturvedi2003@gmail.com>
2025-06-06 02:14:38 +0530

    Model training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,1 +1,84 @@
 #training my ml models here
+import os
+import sys
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree   import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from sklearn.metrics import r2_score
+from src.utlis import save_object
+from src.utlis import evaluate_model
+
+
+#config class give us watever input i require to train my model.it is used to store the configuration of the model trainer
+@dataclass
+class ModelTrainerConfig:   
+    train_model_file_path= os.path.join('artifacts', 'model.pkl')
+
+
+#class where actual training of the model is done
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    # This function initiates the model training process. It takes in training and testing data arrays,
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and testing input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+
+            models = {
+                "LinearRegression": LinearRegression(),
+                "DecisionTreeRegressor": DecisionTreeRegressor(),
+                "RandomForestRegressor": RandomForestRegressor(),
+                "GradientBoostingRegressor": GradientBoostingRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "KNeighborsRegressor": KNeighborsRegressor(),
+                "CatBoostRegressor": CatBoostRegressor(verbose=False),
+                "AdaBoostRegressor": AdaBoostRegressor(),
+            }
+
+            model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,)
+            
+            best_model_score = max(model_report.values())
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            
+
+            best_model = models[best_model_name]
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
+
+            save_object(
+                file_path=self.model_trainer_config.train_model_file_path,
+                obj=best_model,
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+
+            return r2_square, best_model_name
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
